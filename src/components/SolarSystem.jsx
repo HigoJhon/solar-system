3a929621f20da269e01b869c6c39fc3d9c7ccc5d
HigoJhon <higojhon98@gmail.com>
2022-10-21 22:18:00 -0300

    Renderizando uma lista de planetas dentro de SolarSystem

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
@@ -7,9 +10,26 @@ class SolarSystem extends React.Component {
       <>
         <div data-testid="solar-system" />
         <Title headline="Planetas" />
+        <ul>
+          {planets
+            .map(({ name, image }) => (
+              <PlanetCard planetName={ name } key={ name } planetImage={ image } />
+            ))}
+        </ul>
       </>
     );
   }
 }
+
+SolarSystem.propTypes = {
+  Planets: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+planets.defaultPtops = {
+  Planets: [],
+};
 
 export default SolarSystem;
